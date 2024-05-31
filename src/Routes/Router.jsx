823744d1607c55ed5ff43f6eef3a2248c8907163
aayman01 <ayman.alfi098@gmail.com>
2024-05-31 20:20:33 +0600

    first commit

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home/Home";
+import Main from "../Layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+    children : [
+        {
+            path : '/',
+            element : <Home/>
+        },
+    ]
+  },
+]);
