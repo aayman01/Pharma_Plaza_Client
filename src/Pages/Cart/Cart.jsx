40915a8eacbfde62586b7157eba5a7179b7983d2
aayman01 <ayman.alfi098@gmail.com>
2024-06-05 23:07:09 +0600

    delete all functionality created in cart

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -7,11 +7,13 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 import QuantityButton from "../Shared/Quantity Button/QuantityButton";
 import { useEffect, useState } from "react";
+import useAuth from "../../Hooks/useAuth";
 
 const Cart = () => {
   const { carts, isLoading, refetch } = useCart();
   const [cartItems, setCartItems] = useState([]);
   const axiosSecure = useAxiosSecure();
+  const {user} = useAuth();
  
   console.log(cartItems)
   
@@ -43,7 +45,6 @@ const Cart = () => {
     );
   };
 
-  console.log(cartItems)
   const handleSingleDelete = (id) => {
     console.log(id)
     Swal.fire({
@@ -69,8 +70,40 @@ const Cart = () => {
         });
       }
     });
-
   }
+
+  const handleAllDelete = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/cart/${user.email}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Successfully Deleted All Your Cart",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  }
+
+  // const handlePay = () => {
+  //   const cartItems = {
+  //     userEmail : user.email,
+      
+
+  //   }
+  // }
   if (isLoading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
@@ -78,75 +111,93 @@ const Cart = () => {
       </div>
     );
   }
+ 
   return (
     <>
       <NavBar />
-      <div className="max-w-6xl mx-auto px-4">
-        <div>
-          <h2 className="text-3xl font-bold text-center my-10">My Carts</h2>
-          <p className="text-3xl font-bold mb-5">
-            Total Selected Item: {carts.length}
-          </p>
+      {cartItems.length === 0 ? (
+        <div className="h-screen-minus-20px flex items-center justify-center">
+          <h2 className="text-3xl font-bold">
+            You have not added anything in your cart
+          </h2>
         </div>
-        <div className="overflow-x-auto border rounded-lg mb-10">
-          <table className="table mt-5">
-            <thead>
-              <tr className="text-xl">
-                <th>#</th>
-                <th>Name</th>
-                <th>Company</th>
-                <th>Price Per Unit</th>
-                <th>Quantity</th>
-                <th>Total Price</th>
-                <th>Remove</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cartItems.map((cart, idx) => (
-                <tr key={cart._id}>
-                  <td className="font-bold">{idx + 1}</td>
-                  <td>
-                    <div className="flex items-center gap-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                          <img src={cart.image} alt="item image" />
+      ) : (
+        <div className="max-w-6xl mx-auto px-4">
+          <div>
+            <h2 className="text-3xl font-bold text-center my-10 underline">
+              My Carts
+            </h2>
+          </div>
+          <div className="flex items-center justify-between mb-5">
+            <p className="text-3xl font-bold">
+              Total Selected Item: {carts.length}
+            </p>
+            <button
+              onClick={handleAllDelete}
+              className="btn text-white bg-red-600 hover:bg-red-800"
+            >
+              <FaTrash /> Clear All Cart
+            </button>
+          </div>
+          <div className="overflow-x-auto border rounded-lg mb-10">
+            <table className="table mt-5">
+              <thead>
+                <tr className="text-xl">
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Company</th>
+                  <th>Price Per Unit</th>
+                  <th>Quantity</th>
+                  <th>Total Price</th>
+                  <th>Remove</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((cart, idx) => (
+                  <tr key={cart._id}>
+                    <td className="font-bold">{idx + 1}</td>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img src={cart.image} alt="item image" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{cart.name}</div>
                         </div>
                       </div>
-                      <div>
-                        <div className="font-bold">{cart.name}</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="font-medium">{cart.companyName}</td>
-                  <td className="font-medium text-left">
-                    {cart?.pricePerUnit}
-                  </td>
-                  <td>
-                    <QuantityButton
-                      quantity={cart.quantity}
-                      onQuantityChange={(newQuantity) =>
-                        handleQuantityChange(cart._id, newQuantity)
-                      }
-                      
-                    ></QuantityButton>
-                  </td>
-                  <td className="font-medium">
-                    {(cart.totalPrice).toFixed(2)}
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => handleSingleDelete(cart._id)}
-                      className="text-red-600 p-2 text-xl hover:bg-slate-100 rounded-lg"
-                    >
-                      <FaTrash />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                    </td>
+                    <td className="font-medium">{cart.companyName}</td>
+                    <td className="font-medium text-left">
+                      {cart?.pricePerUnit}
+                    </td>
+                    <td>
+                      <QuantityButton
+                        quantity={cart.quantity}
+                        onQuantityChange={(newQuantity) =>
+                          handleQuantityChange(cart._id, newQuantity)
+                        }
+                      ></QuantityButton>
+                    </td>
+                    <td className="font-medium">
+                      {cart.totalPrice.toFixed(2)}
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => handleSingleDelete(cart._id)}
+                        className="text-red-600 p-2 text-xl hover:bg-slate-100 rounded-lg"
+                      >
+                        <FaTrash />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      )}
       <Footer />
     </>
   );
