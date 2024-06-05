b04a7e77df4346ba37dd7ec77589a389f0a262ac
aayman01 <ayman.alfi098@gmail.com>
2024-06-05 12:39:04 +0600

    axios interceptor created

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,44 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
 
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+});
 const useAxiosSecure = () => {
-    const axiosSecure = axios.create({
-      baseURL: "http://localhost:5000",
-    });
-    return axiosSecure;
+    
+    const navigate = useNavigate();
+    const { logOut } = useAuth();
+
+    axiosSecure.interceptors.request.use(
+      function (config) {
+        const token = localStorage.getItem("access-token");
+        // console.log('req token',token)
+        config.headers.authorization = `bearer ${token}`;
+        return config;
+      },
+      function (error) {
+        return Promise.reject(error);
+      }
+    );
+
+    // interceptor response
+    axiosSecure.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      async (error) => {
+        const status = error.response?.status;
+        console.log("error code in interceptor", status);
+        if (status === 401 || status === 403) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+    return axiosSecure;  
 };
 
 export default useAxiosSecure;
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -3,6 +3,8 @@ import { ClipLoader } from "react-spinners";
 import Table from "../Shared/Table/Table";
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import NavBar from "../Shared/NavBar/NavBar";
+import Footer from "../Shared/Footer/Footer";
 
 const Shop = () => {
   const [searchText, setSearchText] = useState('');
@@ -53,108 +55,112 @@ const Shop = () => {
   }
   
   return (
-    <div className="max-w-6xl mx-auto px-4">
-      <div className="text-center mt-8">
-        <h2 className="text-4xl font-bold mb-2">All Products</h2>
-        <p className="text-gray-500 text-sm mb-12">
-          Find All Your Product You Need
-        </p>
-      </div>
-      <div className="flex items-center justify-center gap-7 px-10 pb-10">
-        {/* search */}
-        <form
-          onSubmit={handleSearch}
-          className="flex items-center justify-center gap-1"
-        >
-          <input
-            type="text"
-            placeholder="Type here"
-            onChange={(e) => setSearchText(e.target.value)}
-            value={searchText}
-            className="input input-bordered w-full"
-          />
-          <input
-            className="btn text-white bg-[#076cec] hover:bg-[#0072CE]"
-            type="submit"
-            value="Search"
-          />
-        </form>
-        {/* sort */}
-        <div>
-          <select
-            onChange={(e) => {
-              setSort(e.target.value);
-              refetch();
-            }}
-            defaultValue=""
-            value={sort}
-            className="select select-bordered w-full"
+    <>
+    <NavBar/>
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="text-center mt-8">
+          <h2 className="text-4xl font-bold mb-2">All Products</h2>
+          <p className="text-gray-500 text-sm mb-12">
+            Find All Your Product You Need
+          </p>
+        </div>
+        <div className="flex items-center justify-center gap-7 px-10 pb-10">
+          {/* search */}
+          <form
+            onSubmit={handleSearch}
+            className="flex items-center justify-center gap-1"
           >
-            <option disabled value="">
-              Sort by price
-            </option>
-            <option value="dsc">Price: low to high</option>
-            <option value="asc">Price: high to low</option>
-          </select>
+            <input
+              type="text"
+              placeholder="Type here"
+              onChange={(e) => setSearchText(e.target.value)}
+              value={searchText}
+              className="input input-bordered w-full"
+            />
+            <input
+              className="btn text-white bg-[#076cec] hover:bg-[#0072CE]"
+              type="submit"
+              value="Search"
+            />
+          </form>
+          {/* sort */}
+          <div>
+            <select
+              onChange={(e) => {
+                setSort(e.target.value);
+                refetch();
+              }}
+              defaultValue=""
+              value={sort}
+              className="select select-bordered w-full"
+            >
+              <option disabled value="">
+                Sort by price
+              </option>
+              <option value="dsc">Price: low to high</option>
+              <option value="asc">Price: high to low</option>
+            </select>
+          </div>
+        </div>
+        <div className="overflow-x-auto mb-20 border rounded-lg">
+          <table className="table">
+            <thead>
+              <tr className="text-lg text-[#076cec]">
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Company Name</th>
+                <th>Price Per Unit</th>
+                <th>Add to Cart</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, idx) => (
+                <Table key={idx} idx={idx} product={product}></Table>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {/* pagination */}
+        <div className="flex justify-center my-12 ">
+          {/* Previous Button */}
+          <button
+            disabled={currentPage === 1}
+            onClick={() => handlePaginationButton(currentPage - 1)}
+            className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white"
+          >
+            <div className="flex items-center -mx-1">
+              <span className="mx-1 font-medium">previous</span>
+            </div>
+          </button>
+          {/* Numbers */}
+          {pages.map((btnNum) => (
+            <button
+              key={btnNum}
+              onClick={() => handlePaginationButton(btnNum)}
+              className={`hidden ${
+                currentPage === btnNum
+                  ? "bg-[#076cec] text-white font-medium"
+                  : ""
+              } px-4 py-2 mx-1 transition-colors duration-300 transform font-medium rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            >
+              {btnNum}
+            </button>
+          ))}
+          {/* Next Button */}
+          <button
+            disabled={currentPage === numberOfPages}
+            onClick={() => handlePaginationButton(currentPage + 1)}
+            className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+          >
+            <div className="flex items-center -mx-1">
+              <span className="mx-1 font-medium">Next</span>
+            </div>
+          </button>
         </div>
       </div>
-      <div className="overflow-x-auto mb-20 border rounded-lg">
-        <table className="table">
-          <thead>
-            <tr className="text-lg text-[#076cec]">
-              <th>#</th>
-              <th>Product Name</th>
-              <th>Company Name</th>
-              <th>Price Per Unit</th>
-              <th>Add to Cart</th>
-              <th>Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product, idx) => (
-              <Table key={idx} idx={idx} product={product}></Table>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {/* pagination */}
-      <div className="flex justify-center my-12 ">
-        {/* Previous Button */}
-        <button
-          disabled={currentPage === 1}
-          onClick={() => handlePaginationButton(currentPage - 1)}
-          className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white"
-        >
-          <div className="flex items-center -mx-1">
-            <span className="mx-1 font-medium">previous</span>
-          </div>
-        </button>
-        {/* Numbers */}
-        {pages.map((btnNum) => (
-          <button
-            key={btnNum}
-            onClick={() => handlePaginationButton(btnNum)}
-            className={`hidden ${
-              currentPage === btnNum
-                ? "bg-[#076cec] text-white font-medium"
-                : ""
-            } px-4 py-2 mx-1 transition-colors duration-300 transform font-medium rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
-          >
-            {btnNum}
-          </button>
-        ))}
-        {/* Next Button */}
-        <button
-          disabled={currentPage === numberOfPages}
-          onClick={() => handlePaginationButton(currentPage + 1)}
-          className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
-        >
-          <div className="flex items-center -mx-1">
-            <span className="mx-1 font-medium">Next</span>
-          </div>
-        </button>
-      </div>
-    </div>
+      <Footer/>
+    </>
   );
 };
 
