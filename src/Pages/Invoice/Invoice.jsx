f08ee874ee79ab0303e70a37201917262539b93e
aayman01 <ayman.alfi098@gmail.com>
2024-06-07 04:07:26 +0600

    working on invoice

--- src/Pages/Invoice/Invoice.jsx
+++ src/Pages/Invoice/Invoice.jsx
@@ -1,15 +1,31 @@
 import Footer from "../Shared/Footer/Footer";
 import NavBar from "../Shared/NavBar/NavBar";
 import logo from '../../assets/icon.png';
+import { useEffect } from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const Invoice = () => {
+    const axiosSecure = useAxiosSecure();
+    useEffect(() =>{
+        axiosSecure.get("/invoice")
+        .then(res => {
+            console.log(res.data);
+        })
+    },[axiosSecure])
     return (
         <div>
             <NavBar></NavBar>
             <div className="max-w-6xl mx-auto px-4">
                 <div>
-                    <div><img src={logo} alt="" /></div>
-                    <div></div>
+                    <div className="mt-10 mb-8">
+                        <h2 className="text-3xl font-bold text-center underline decoration-blue-600">Invoice Page</h2>
+                    </div>
+                    <div>
+                        <img className="w-56  h-8" src={logo} alt="" />
+                    </div>
+                    <div>
+                        <p>Invoice no : {}</p>
+                    </div>
                 </div>
             </div>
             <Footer></Footer>
